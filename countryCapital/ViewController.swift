338836761627f99128dae02aec8515b03ff69c43
Nietzsky <Nietzsky@hive.fritz.box>
2017-09-01 18:25:38 +0200

    Initial Commit

---
+++ countryCapital/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  countryCapital
+//
+//  Created by Nietzsky on 01/09/2017.
+//  Copyright © 2017 Testerik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
